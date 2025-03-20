8272776c3d4e75472a96adab72745c89e0eb362e
Ivan-Yarl <germanovich89@mail.ru>
2025-03-20 19:56:47 +0300

    config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,8 @@
 const config = {
     clientId: '1bcd872b-594c-8022-b12e-003762f82665',
-    // Обновляем redirectUri согласно реальному URL
-    redirectUri: 'https://ivan-yarl.github.io/voicepen-legal/callback',
+    clientSecret: 'secret_6YDhaKRxUgvuRe1ZX1EeXGDnlLpMzfiO6S51k8oV6BG',
     notionAuthUrl: 'https://api.notion.com/v1/oauth/authorize',
-    notionTokenUrl: 'https://api.notion.com/v1/oauth/token'
+    notionTokenUrl: 'https://api.notion.com/v1/oauth/token',
+    redirectUri: 'https://ivan-yarl.github.io/voicepen-legal/callback',
+    databaseId: 'e0c400b77e644a7bb8c3d3e4bc3c5b91'
 };
