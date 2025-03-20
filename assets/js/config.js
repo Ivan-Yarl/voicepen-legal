c3875daedc926dab8042fa54d3b922978842a602
Ivan-Yarl <germanovich89@mail.ru>
2025-03-20 19:10:09 +0300

    config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const config = {
-    clientId: 'ваш_client_id_из_notion',
-    redirectUri: 'https://ivan-yarl.github.io/voicepen-legal/callback',
+    clientId: '1bcd872b-594c-8022-b12e-003762f82665',
+    redirectUri: 'https://ivan_yarl.github.io/voicepen-legal-main/callback',
     notionAuthUrl: 'https://api.notion.com/v1/oauth/authorize',
     notionTokenUrl: 'https://api.notion.com/v1/oauth/token'
 };
