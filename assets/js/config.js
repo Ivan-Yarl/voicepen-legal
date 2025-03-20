f09a2ef5d2cb9b14265ed7a695d0d0ce4fdf3133
Ivan-Yarl <germanovich89@mail.ru>
2025-03-20 20:21:46 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const config = {
     clientId: '1bcd872b-594c-8022-b12e-003762f82665',
-    clientSecret: 'secret_6YDhaKRxUgvuRe1ZX1EeXGDnlLpMzfiO6S51k8oV6BG',
+    clientSecret: '>>>',
     notionAuthUrl: 'https://api.notion.com/v1/oauth/authorize',
     notionTokenUrl: 'https://api.notion.com/v1/oauth/token',
     redirectUri: 'https://ivan-yarl.github.io/voicepen-legal/callback',
