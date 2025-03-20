3787628d1e97e08a19b9a98e21ef2cac137e3fee
Ivan-Yarl <germanovich89@mail.ru>
2025-03-20 16:36:44 +0300

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+const config = {
+    clientId: 'ваш_client_id_из_notion',
+    redirectUri: 'https://ivan-yarl.github.io/voicepen-legal/callback',
+    notionAuthUrl: 'https://api.notion.com/v1/oauth/authorize',
+    notionTokenUrl: 'https://api.notion.com/v1/oauth/token'
+};
